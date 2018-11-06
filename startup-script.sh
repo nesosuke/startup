9f85bc508b3362f990dac6cbc1ab3775c35adc91
nesosuke <nesosuke@gmail.com>
2018-11-06 23:35:32 +0900

    fix

--- startup-script.sh
+++ startup-script.sh
@@ -18,13 +18,15 @@ mkdir ~/git
 git clone https://github.com/nesosuke/dotfiles_assign ~/git/dotfiles_assgin
 cd ~/git/dotfiles_assign
 bash dotfiles_assign.sh
-cd -
+cd ~/
 
 #Download GRAV
-git clone https://github.com/download/core/grav-admin/1.5.4 -o grav-admin-1.5.4.zip
+wget https://github.com/download/core/grav-admin/1.5.4 -o grav-admin-1.5.4.zip
+echo ""
 echo "grav-admin.zip was downloaded! Unzip and install!"
 
 
 #display rest steps
+echo ""
 echo "Your rest work --> fix sshd_config: DISABLE PermitRootLogin  / CHANGE port 22 / REGISTER your pubkey" 
 
